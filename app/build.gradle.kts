a71742f376c5eb2a7d61ee2212bad837a06ba8cc
wamweya123-cyber <wamweya123@gmail.com>
2026-08-15 05:24:14 +0300

    Add Retrofit and Moshi dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
-}
+}implementation("com.squareup.retrofit2:retrofit:2.11.0")
+implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+implementation("com.squareup.okhttp3:okhttp:4.12.0")
+implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
