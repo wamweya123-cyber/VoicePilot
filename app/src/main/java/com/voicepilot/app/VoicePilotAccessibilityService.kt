4d1b1fcc4a47d2cba2933a8afff8817eb11fb008
wamweya123-cyber <wamweya123@gmail.com>
2026-08-15 06:32:04 +0300

    Add VoicePilotAccessibilityService class
    Implement VoicePilotAccessibilityService to handle accessibility events.

--- app/src/main/java/com/voicepilot/app/VoicePilotAccessibilityService.kt
+++ app/src/main/java/com/voicepilot/app/VoicePilotAccessibilityService.kt
@@ -1,1 +1,15 @@
+package com.voicepilot.app
 
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class VoicePilotAccessibilityService : AccessibilityService() {
+
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
+        // Accessibility events will be handled here.
+    }
+
+    override fun onInterrupt() {
+        // Called when the service is interrupted.
+    }
+}
